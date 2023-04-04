c611a223e9ec0a9f60a573aec028b3ff757bf08e
owinoalfred <ogzialfred@gmail.com>
2023-04-04 11:39:59 +0300

    https://github.com/owinoalfred/htdocs.git

---
+++ src1/index.php
@@ -0,0 +1,13 @@
+<p>
+    This is a paragraph but below there is PHP
+</p>
+
+<?php
+echo "<h1>PHP is realy hard</h1>";
+echo "<h2>Hello  World<h2>";
+if (1==1){
+    print "<h1>yes this is the main thing that can be of the last ones that i do</h1>";
+    echo "What can 1 be equal to 0 ";
+    echo 4+3;
+}
+?>
